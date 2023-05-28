69f1fa627350877dff484bf07cb429f99ab26cc5
Bruno Andrade <bruno.faria.andrade@gmail.com>
2023-05-28 19:24:28 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), viteSingleFile()],
 })
