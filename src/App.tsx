f0d2dceceec6865599b5419bc2eafa2676fe4ca9
Bruno Andrade <bruno.faria.andrade@gmail.com>
2023-06-08 18:46:13 -0300

    chore: refactor and adjusted global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { DualMonitor } from 'components/DualMonitor'
 import { createGlobalStyle } from 'styled-components'
 
+import { DualMonitor } from 'components/DualMonitor'
+
 export const GlobalStyles = createGlobalStyle`
-  
+
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
   html {
     font-size: 62.5%
   }
 
   body {
-    margin: 0;
-    padding: 0;
     background-color: black;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     font-size: 1.6rem;
