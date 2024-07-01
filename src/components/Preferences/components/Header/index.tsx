bdaaa5f9cf35fdd9c11fed08836f928d868a59ca
Bruno Andrade <bruno.faria.andrade@gmail.com>
2024-06-30 22:15:51 -0300

    fix: AMD gpu label name

--- src/components/Preferences/components/Header/index.tsx
+++ src/components/Preferences/components/Header/index.tsx
@@ -40,7 +40,7 @@ export const Header = () => {
     <Container>
       <div className="info">
         <div className="title">
-          Dual Info <span>v1.6.0</span>
+          Dual Info <span>v1.6.1</span>
         </div>
 
         <div className="author">
